80199bd7988c1e18c5aceda893e8329e2569b9ca
Johannes Hingerl <johannes.hingerl@tum.de>
2025-01-17 13:28:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='borzoi-pytorch',
-    version='0.4.0',
+    version='0.4.1',
     author='Johannes Hingerl',
     author_email='johannes.hingerl@tum.de',
     packages=['borzoi_pytorch'],
