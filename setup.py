9070dfb0371ca086c1d38ff8e2317b2ed0c8400e
Johannes Hingerl <johannes.hingerl@tum.de>
2025-06-30 13:37:11 -0700

    Fixed transformer versioning for now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='borzoi-pytorch',
-    version='0.4.2',
+    version='0.4.3',
     author='Johannes Hingerl',
     author_email='johannes.hingerl@tum.de',
     packages=['borzoi_pytorch'],
@@ -15,7 +15,7 @@ setup(
         "einops >= 0.5",
         "numpy >= 1.14.2",
         "torch >= 2.1.0",
-        "transformers >= 4.34.1",
+        "transformers >= 4.34.1,<4.51.0",
         "jupyter >= 1.0.0; extra == 'dev'",
 	"intervaltree~=3.1.0",
 	"pandas",
